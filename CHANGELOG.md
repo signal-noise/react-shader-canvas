cc9036fc7e000fdce28b640809b38a250347655c
John Chipps-Harding <john@signal-noise.co.uk>
2019-11-18 18:53:56 +0000

    Optimized WebGL Check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project are documented in this file.
 
+## 0.0.3
+
+- Changed: Optimized WebGL Check.
+
 ## 0.0.2
 
 - Added: Ability to pass styles in.
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,14 @@
-export const getDevicePixelRatio = () => {
-  if (typeof document === "undefined") return 1;
-  return devicePixelRatio || 1;
-};
+let supported = undefined;
 
 export const isWebGlSupported = () => {
   if (typeof document === "undefined") return false;
-  return !!document.createElement("canvas").getContext("webgl");
+  if (supported === undefined) {
+    supported = !!document.createElement("canvas").getContext("webgl");
+  }
+  return supported;
+};
+
+export const getDevicePixelRatio = () => {
+  if (typeof document === "undefined") return 1;
+  return devicePixelRatio || 1;
 };
